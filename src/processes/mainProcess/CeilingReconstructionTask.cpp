077b7e85de74f21b4ae352576f698236d312034b
Pedro Caversan <pedrocaversanpassos@gmail.com>
2026-05-16 19:33:56 -0300

    feat: finaliza implementação mínima da thread de reconstrução do teto

--- src/processes/mainProcess/CeilingReconstructionTask.cpp
+++ src/processes/mainProcess/CeilingReconstructionTask.cpp
@@ -3,12 +3,17 @@
 #include <unistd.h>
 
 #include <chrono>
+#include <cmath>
 #include <thread>
 
 #include "utils/coord_buffer.hpp"
 #include "utils/pos_buffer.hpp"
 
-// Etapa 1
+int i_lidar = 200;
+
+void simulateLidarSensor(double t) {
+    i_lidar = static_cast<int>(200 + 100 * std::cos(t));
+}
 
 // Filtro EMA (Exponential Moving Average) foi escolhido no lugar de SMA (Simple
 // Moving Average) por também suavizar os dados, mas dar peso maior a dados
@@ -20,38 +25,40 @@ struct EMAFilter {
 };
 
 float filterValue(EMAFilter& f, float new_value) {
-    // implementação do filtro
+    // TODO (Pedro): implementação do filtro
     return new_value;
 }
-
-struct RefinedData {
-        int x;
-        int y;
-        std::chrono::steady_clock::time_point
-            time_stamp;  // verificar o tipo das variáveis
-};
 
 void ceilingReconstructionHandler(std::binary_semaphore& x_was_sent,
                                   PosBuffer& pos_buf, CoordBuffer& coord_buf) {
     EMAFilter f_x;
     EMAFilter f_y;
-    auto next_wake = std::chrono::steady_clock::now();
+    auto task_start = std::chrono::steady_clock::now();
+    auto next_wake = task_start;
+    // loop da task
     while (true) {
         next_wake += std::chrono::milliseconds(100);
         std::this_thread::sleep_until(next_wake);
 
-        RefinedData refined_data = {0};
-
-        x_was_sent.acquire();  // garante que o valor da coordenada no buffer de
-                               // cima já foi atualizado
+        x_was_sent.acquire();  // garante que o valor da coordenada no
+        // buffer de cima já foi atualizado antes de consumi-lo
         int x_coord = std::get<PosData>(pos_buf.consumer()).pos;
 
-        int y_coord = 1;  // aqui eu leio o valor percebido pelo lidar
+        double t = std::chrono::duration<double>(
+                       std::chrono::steady_clock::now() - task_start)
+                       .count();
+        simulateLidarSensor(t); // simulação para testes
+        int y_coord =
+            i_lidar;  // aqui deve ser lido o valor percebido pelo lidar
 
-        refined_data.x = filterValue(f_x, x_coord);
-        refined_data.y = filterValue(f_y, y_coord);
-        refined_data.time_stamp = std::chrono::steady_clock::now();
+        CoordData refined_data = {std::chrono::system_clock::now(), {0, 0}};
+        refined_data.coord[0] = filterValue(f_x, x_coord);
+        ;
+        refined_data.coord[1] = filterValue(f_y, y_coord);
+        ;
 
-        // aqui a refined_data deve ser enviada ao buffer de baixo
+        // envio dos dados percebidos pelo lidar e encoder ao buffer de
+        // coordenadas
+        coord_buf.producer(refined_data);
     }
 }
